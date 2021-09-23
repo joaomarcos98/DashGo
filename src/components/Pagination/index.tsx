7e867ebe8c4f24d71fb9fc5db933fdb4c9d0ab9c
Joao <joomarcos.belanga98@gmail.com>
2021-09-23 09:42:21 -0300

    Responsive

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -4,20 +4,20 @@ import { PaginationItem } from "./PaginationItem";
 export function Pagination() {
     return (
         <Stack
-            direction="row"
+            direction={["column", "row"]}
             mt="8"
             justify="space-between"
             align="center"
         >
             <Box>
-            <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+                <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
             </Box>
             <Stack direction="row" spacing='2'>
-               <PaginationItem number={1} isCurrent/>
-               <PaginationItem number={2} />
-               <PaginationItem number={3} />
-               <PaginationItem number={4} />
-               <PaginationItem number={5} />
+                <PaginationItem number={1} isCurrent />
+                <PaginationItem number={2} />
+                <PaginationItem number={3} />
+                <PaginationItem number={4} />
+                <PaginationItem number={5} />
             </Stack>
         </Stack>
     )
--- src/contexts/SidebarDrawerContext.tsx
+++ src/contexts/SidebarDrawerContext.tsx
@@ -1,5 +1,6 @@
 import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
-import { createContext, ReactNode, useContext } from "react";
+import { useRouter } from "next/dist/client/router";
+import { createContext, ReactNode, useContext, useEffect } from "react";
 
 interface SidebarDrawerProviderProps {
     children: ReactNode
@@ -10,8 +11,13 @@ type SidebarDrawerContextData = UseDisclosureReturn
 const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
 
 export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
-
     const disclosure = useDisclosure()
+    const router = useRouter()
+
+    useEffect(() => {
+        disclosure.onClose()
+    }, [router.asPath])
+
     return (
         <SidebarDrawerContext.Provider value={disclosure}>
             {children}
