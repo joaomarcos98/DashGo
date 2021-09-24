11cff25ebaf1acdd689b216b8e78a9e20e820f58
Joao <joomarcos.belanga98@gmail.com>
2021-09-24 14:41:36 -0300

    Adding mirageJS

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,15 +1,24 @@
 import { Box, Flex, Heading, Button, Icon, Table, Thead, Tbody, Tr, Th, Checkbox, Td, Text, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+
 
 export default function UserList() {
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
     })
+
+    useEffect(()=> {
+        fetch("http://localhost:3000/api/users")
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }, [])
 
     return (
         <Box>
